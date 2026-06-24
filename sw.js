1180fe4760db24d549c0435cb5b816f8cd066500
Hernan <59187022+nastasuper@users.noreply.github.com>
2026-06-24 17:07:32 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'improver-v1';
+const FILES = [
+  './barry_harris.html',
+  './barry_harris_m2.html',
+  './manifest.json',
+  './icon.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request)));
+});
